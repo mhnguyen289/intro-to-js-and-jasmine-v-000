748211b72e728c220a2d9e691de80474ed5f77d8
Minh N <mhnguyen289@gmail.com>
2016-03-25 03:46:27 +0000

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,11 @@
 'use strict';
+function favIceCream(name_of_icecream)
+return "I love " + name_of_icecream
+
+function shouting(shouting_loud)
+return shouting_loud.upcase
+
+function roundDown(number)
+Math.floor.number
+
+function theTruth
